f6bbb7894dc17f6132131947f9a2d534afb99a2c
Isaac_Santos <isaacdsantos@hotmail.com>
2023-11-14 18:31:42 -0300

    Criada funcionalidade de embaralho para shuffleEmojis

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -1,4 +1,4 @@
-const emoji = [
+const emojis = [
     "🐱",
     "🐱",
     "🦝",
@@ -18,3 +18,13 @@ const emoji = [
 ];
 let openCards =[];
 
+let shuffleEmojis = emojis.sort( ()=>( Math.random() > 0.5 ? 2 : -1 ) );
+
+for(let i=0; i< emojis.length; i++){
+    let box = document.createElement
+                ("div");
+                box.className = "item";
+                box.innerHTML = shuffleEmojis[i];
+                document.querySelector(".game")
+                .appendChild(box);
+}
